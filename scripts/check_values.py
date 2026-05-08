00d76526df7b93594dd6f0a858cf6e03e0200159
OyeLurker <oyelurker@gmail.com>
2026-05-08 21:58:00 +0530

    Cleanup: Remove helper scripts and temporary lock files from repository

--- scripts/check_values.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-df = pd.read_excel('attackmitre.xlsx')
-print(df[['Group Techniques', 'APT Group Name', 'Software ID']].head(10))
-
-df2 = pd.read_excel('MitreEnterprise.xlsx')
-print(df2[['Tactic ID', 'Tactic Name']].head(10))
